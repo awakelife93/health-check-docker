69f349daa8a080265bc6463bcc7f62678d5c03c4
hw.park <hw.park@fastfive.co.kr>
2022-08-03 19:02:00 +0900

    scheduler module 추가

---
+++ worker/scheduler.go
@@ -0,0 +1,54 @@
+package worker
+
+import (
+	"fmt"
+	"time"
+)
+
+// * interval timer channel
+var quitChannel = make(chan bool)
+
+// * ticker
+var ticker *time.Ticker
+
+func quitIntervalAction() bool {
+	fmt.Println("Call QuitIntervalAction")
+	quit := <-quitChannel
+	return quit
+}
+
+func StartScheduler(second time.Duration, delay time.Duration, action func()) chan bool {
+	var defaultSecond time.Duration = 30
+	if second < defaultSecond {
+		second = defaultSecond
+	}
+
+	ticker := time.NewTicker(second * time.Second)
+
+	action()
+
+	for {
+		select {
+		case <-time.After(delay):
+		case <-ticker.C:
+			go action()
+		case <-quitChannel:
+			ticker.Stop()
+			return quitChannel
+		}
+	}
+}
+
+func ClearTicker() {
+	fmt.Println("Quit interval Action = ", quitIntervalAction())
+	ticker = nil
+}
+
+func ClearIntervalChannel() {
+	quitChannel = nil
+}
+
+func ClearScheduler() {
+	ClearTicker()
+	ClearIntervalChannel()
+}
